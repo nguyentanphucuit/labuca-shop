017ace78202dfad5bf8afd81d8972c274e743af9
KID <nguyentanphucuit@gmail.com>
2024-12-14 23:21:38 +0700

    update GTM

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -10,8 +10,8 @@ const Header = () => {
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','5CXDKNWS');
-      `}
+        })(window,document,'script','dataLayer','GTM-5CXDKNWS');
+        `}
       </script>
     </head>
   );
