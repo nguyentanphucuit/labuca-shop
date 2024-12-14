41a0860d2bd0ec5bae1d62c910ef5486ec8f01d0
KID <nguyentanphucuit@gmail.com>
2024-12-14 23:12:20 +0700

    update GTM

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,19 @@
+import Script from "next/script";
 import React from "react";
 
 const Header = () => {
   return (
     <head>
       <link rel="icon" href="/labuca.svg" sizes="any" />
+      <Script id="gtm" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','KLXH5B7D');
+      `}
+      </Script>
     </head>
   );
 };
