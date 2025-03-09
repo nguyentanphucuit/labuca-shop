05b0c8a268b7d277e671180f0a12d6669f5788bb
KID <nguyentanphucuit@gmail.com>
2025-03-09 12:09:41 +0700

    update color cart

--- src/app/components/ProductDetails.tsx
+++ src/app/components/ProductDetails.tsx
@@ -49,7 +49,7 @@ const ProductDetails = ({
           <Link
             href={href}
             target="_blank"
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-500 rounded-lg hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-700">
             <svg
               className="w-4 h-4 text-white dark:text-gray-800"
               aria-hidden="true"
