d6c35158794aec5b003c9dbb0618df33cead4b1b
KID <nguyentanphucuit@gmail.com>
2025-05-18 00:53:20 +0700

    update favicon ico

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,16 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <head>
-      <link rel="icon" href="/labuca-2.svg" sizes="any" />
-      {/* <script>
-        {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-5CXDKNWS');
-        `}
-      </script> */}
+      <link rel="icon" href="/favicon.ico" sizes="32x32" />
     </head>
   );
 };
